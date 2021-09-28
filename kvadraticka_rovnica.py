51e17d1501f31fe790b2af4cbecada34b2661401
MiaDudasova <91089971+MiaDudasova@users.noreply.github.com>
2021-09-28 09:31:23 +0200

    Update kvadraticka_rovnica.py

--- kvadraticka_rovnica.py
+++ kvadraticka_rovnica.py
@@ -1,20 +1,21 @@
-import math
-
 a = int(input("Zadaj hodnotu a: "))
 b = int(input("Zadaj hodnotu b: "))
 c = int(input("Zadaj hodnotu c: "))
 
-d = (b*b)-(4*a*c)
-if d > 0:
-    d = math.sqrt(d)
-    x1 = (-b + d)/(2*a)
-    x2 = (-b - d)/(2*a)
-    print("Korene kvadratickej rovnice sú:", int(x1), ",", int(x2))
-elif d == 0:
-    x1 = (-b + d)/(2*a)
-    x2 = (-b - d)/(2*a)
-    print("Koreň kvadratickej rovnice je:", int(x1), ",", int(x2))
-elif d < 0:
-    print("Rovnica má dve rôzne komplexné korene.")
+if a == 0:
+    print("Toto nie je kvadratická rovnica.")
 else:
-    print("Error!")
+    d = (b*b)-(4*a*c)
+    if d > 0:
+        d = d**(1/2)
+        x1 = (-b + d)/(2*a)
+        x2 = (-b - d)/(2*a)
+        print("Korene kvadratickej rovnice sú:", int(x1), ",", int(x2))
+    elif d == 0:
+        x1 = (-b + d)/(2*a)
+        x2 = (-b - d)/(2*a)
+        print("Koreň kvadratickej rovnice je:", int(x1), ",", int(x2))
+    elif d < 0:
+        print("Rovnica má dve rôzne komplexné korene.")
+    else:
+        print("Error!")
