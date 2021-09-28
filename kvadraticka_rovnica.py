0fe1b74a5869c91a9e263f6949339246a71c56f6
MiaDudasova <91089971+MiaDudasova@users.noreply.github.com>
2021-09-28 09:17:40 +0200

    Add files via upload
    Výpočet kvadratickej rovnice

---
+++ kvadraticka_rovnica.py
@@ -0,0 +1,20 @@
+import math
+
+a = int(input("Zadaj hodnotu a: "))
+b = int(input("Zadaj hodnotu b: "))
+c = int(input("Zadaj hodnotu c: "))
+
+d = (b*b)-(4*a*c)
+if d > 0:
+    d = math.sqrt(d)
+    x1 = (-b + d)/(2*a)
+    x2 = (-b - d)/(2*a)
+    print("Korene kvadratickej rovnice sú:", x1, ",", x2)
+elif d == 0:
+    x1 = (-b + d)/(2*a)
+    x2 = (-b - d)/(2*a)
+    print("Koreň kvadratickej rovnice je:", x1, ",", x2)
+elif d < 0:
+    print("Rovnica má dve rôzne komplexné korene.")
+else:
+    print("Error!")
